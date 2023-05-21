7f90a7fd6b6d3ae47c96fa99d56188a98bdcb182
VisalYun <yunvisal18@kit.edu.kh>
2023-05-21 10:05:33 +0900

    add mobile keyboard functionality

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-const NORMAL_CHARACTER = [["«", "១", "២", "៣", "៤", "៥", "៦", "៧", "៨", "៩", "០", "ឥ", "ឲ", "delete"], ["tab", "ឆ", "ិ", "េ", "រ", "ត", "យ", "ុ", "ិ", "ោ", "ផ", "ៀ", "ឪ", "ឮ"], ["caps lock", "ា", "ស", "ដ", "ថ", "ង", "ហ", "្", "ក", "ល", "ើ", "់", "enter"], ["shift", "ឋ", "ខ", "ច", "វ", "ប", "ន", "ម", "ឦ", "។", "៊", "shift"], ["Ctrl", "", "", "Alt", "Space", "Alt", "", "", "Ctrl"]];
-const SHIFT_CHARACTER = [["»", "!", "ៗ", "\"", "៛", "%", "៍", "័", "៏", "(", ")", "៌", "=", "delete"], ["tab", "ឈ", "ឺ", "ែ", "ឬ", "ទ", "ួ", "ូ", "ី", "ៅ", "ភ", "ឿ", "ឧ", "ឭ"], ["caps lock", "ាំ", "ៃ", "ឌ", "ធ", "អ", "ះ", "ញ", "គ", "ឡ", "េាះ", "៉", "enter"], ["shift", "ឍ", "ឃ", "ជ", "េះ", "ព", "ណ", "ំ", "ុះ", "៕", "?", "shift"], ["Ctrl", "", "", "Alt", "Space", "Alt", "", "", "Ctrl"]];
-const ALT_CHARACTER = [["", "", "@", "៑", "$", "€", "៙", "៚", "*", "{", "}", "x", "៎", "delete"], ["tab", "", "", "ឯ", "ឫ", "", "", "", "ឦ", "ឱ", "ឰ", "ឩ", "ឳ", "\\"], ["caps lock", "", "", "", "", "", "", "", "", "", "៖", "◌ៈ", "enter"], ["shift", "", "", "", "", "", "", "", ",", ".", "/", "shift"], ["Ctrl", "", "", "Alt", "Space", "Alt", "", "", "Ctrl"]];
+const DESKTOP_CHARACTER = {
+    NORMAL_CHARACTER: [["«", "១", "២", "៣", "៤", "៥", "៦", "៧", "៨", "៩", "០", "ឥ", "ឲ", "delete"], ["tab", "ឆ", "ិ", "េ", "រ", "ត", "យ", "ុ", "ិ", "ោ", "ផ", "ៀ", "ឪ", "ឮ"], ["caps lock", "ា", "ស", "ដ", "ថ", "ង", "ហ", "្", "ក", "ល", "ើ", "់", "enter"], ["shift", "ឋ", "ខ", "ច", "វ", "ប", "ន", "ម", "ឦ", "។", "៊", "shift"], ["Ctrl", "", "", "Alt", "Space", "Alt", "", "", "Ctrl"]],
+    SHIFT_CHARACTER: [["»", "!", "ៗ", "\"", "៛", "%", "៍", "័", "៏", "(", ")", "៌", "=", "delete"], ["tab", "ឈ", "ឺ", "ែ", "ឬ", "ទ", "ួ", "ូ", "ី", "ៅ", "ភ", "ឿ", "ឧ", "ឭ"], ["caps lock", "ាំ", "ៃ", "ឌ", "ធ", "អ", "ះ", "ញ", "គ", "ឡ", "ោះ", "៉", "enter"], ["shift", "ឍ", "ឃ", "ជ", "េះ", "ព", "ណ", "ំ", "ុះ", "៕", "?", "shift"], ["Ctrl", "", "", "Alt", "Space", "Alt", "", "", "Ctrl"]],
+    ALT_CHARACTER: [["", "", "@", "៑", "$", "€", "៙", "៚", "*", "{", "}", "x", "៎", "delete"], ["tab", "", "", "ឯ", "ឫ", "", "", "", "ឦ", "ឱ", "ឰ", "ឩ", "ឳ", "\\"], ["caps lock", "", "", "", "", "", "", "", "", "", "៖", "◌ៈ", "enter"], ["shift", "", "", "", "", "", "", "", ",", ".", "/", "shift"], ["Ctrl", "", "", "Alt", "Space", "Alt", "", "", "Ctrl"]],
+}
+
+const MOBILE_CHARACTER = {
+    NORMAL_CHARACTER: [["១", "២", "៣", "៤", "៥", "៦", "៧", "៨", "៩", "០", "ឥ", "ឲ"], ["ឆ", "ិ", "េ", "រ", "ត", "យ", "ុ", "ិ", "ោ", "ផ", "ៀ", "ឪ"], ["ា", "ស", "ដ", "ថ", "ង", "ហ", "្", "ក", "ល", "ើ", "់", "ឮ"], ["⇧", "ឋ", "ខ", "ច", "វ", "ប", "ន", "ម", "ឦ", "។", "៊", "⌫"], ["", "", "Space", "", "⏎"]],
+    SHIFT_CHARACTER: [["!", "ៗ", "\"", "៛", "%", "៍", "័", "៏", "(", ")", "៌", "៎"], ["ឈ", "ឺ", "ែ", "ឬ", "ទ", "ួ", "ូ", "ី", "ៅ", "ភ", "ឿ", "ឧ"], ["ាំ", "ៃ", "ឌ", "ធ", "អ", "ះ", "ញ", "គ", "ឡ", "ោះ", "៉", "ឯ"], ["⇧", "ឍ", "ឃ", "ជ", "េះ", "ព", "ណ", "ំ", "ុះ", "៕", "?", "⌫"], ["", "", "Space", "", "⏎"]],
+    ALT_CHARACTER: [["", "", "@", "៑", "$", "€", "៙", "៚", "*", "{", "}", "x", "៎", "delete"], ["tab", "", "", "ឯ", "ឫ", "", "", "", "ឦ", "ឱ", "ឰ", "ឩ", "ឳ", "\\"], ["caps lock", "", "", "", "", "", "", "", "", "", "៖", "◌ៈ", "enter"], ["shift", "", "", "", "", "", "", "", ",", ".", "/", "shift"], ["Ctrl", "", "", "Alt", "Space", "Alt", "", "", "Ctrl"]],
+}
+
+var charset = {};
 
 document.addEventListener("DOMContentLoaded", function () {
     const SPACIAL_KEY = {
         "shift": {
+            domId: "shift_key",
+            handler: onShiftKeyClick
+        },
+        "⇧": {
             domId: "shift_key",
             handler: onShiftKeyClick
         },
@@ -13,6 +27,10 @@ document.addEventListener("DOMContentLoaded", function () {
             handler: onAltKeyClick
         },
         "delete": {
+            domId: "del_key",
+            handler: onDeleteKeyClick
+        },
+        "⌫": {
             domId: "del_key",
             handler: onDeleteKeyClick
         },
@@ -28,6 +46,10 @@ document.addEventListener("DOMContentLoaded", function () {
             domId: "enter_key",
             handler: onEnterKeyClick
         },
+        "⏎": {
+            domId: "enter_key",
+            handler: onEnterKeyClick
+        },
         "Ctrl": {
             domId: "ctrl_key",
             handler: function () { console.log("Ctrl") }
@@ -38,11 +60,23 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
 
+    const LARGE_CHAR = { "ាំ": {}, "ោះ": {}, "េះ": {}, "ុះ": {} }
+
     var inputString = "";
     const textInput = document.getElementById("textInput");
     textInput.value = inputString;
 
-    const keyboard = document.getElementById("keyboard");
+    var keyboard = document.getElementById("desktop-keyboard");
+    console.log(window.getComputedStyle(keyboard).display === "grid")
+    const isDesktop = window.getComputedStyle(keyboard).display == "grid";
+    if (!isDesktop) {
+        keyboard = document.getElementById("mobile-keyboard");
+        charset = MOBILE_CHARACTER;
+    }
+    else {
+        charset = DESKTOP_CHARACTER;
+    }
+
     function changeKey(characters) {
         keyboard.innerHTML = "";
         characters.forEach((row, index) => {
@@ -55,6 +89,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 const keyLabelEle = document.createElement("p");
                 keyLabelEle.classList.add("kh-label");
+                if (LARGE_CHAR[char] && !isDesktop) {
+                    keyLabelEle.style.fontSize = "12px";
+                }
                 keyLabelEle.innerText = char;
 
                 keyEle.appendChild(keyLabelEle);
@@ -100,10 +137,10 @@ document.addEventListener("DOMContentLoaded", function () {
         isShift = !isShift;
         var characters = [];
         if (isShift) {
-            characters = SHIFT_CHARACTER;
+            characters = charset.SHIFT_CHARACTER;
         }
         else {
-            characters = NORMAL_CHARACTER
+            characters = charset.NORMAL_CHARACTER
         }
 
         changeKey(characters);
@@ -114,10 +151,10 @@ document.addEventListener("DOMContentLoaded", function () {
         isAlt = !isAlt;
         var characters = [];
         if (isAlt) {
-            characters = ALT_CHARACTER;
+            characters = charset.ALT_CHARACTER;
         }
         else {
-            characters = NORMAL_CHARACTER
+            characters = charset.NORMAL_CHARACTER
         }
 
         changeKey(characters);
@@ -129,5 +166,5 @@ document.addEventListener("DOMContentLoaded", function () {
     const altKey = document.getElementById("alt_key");
     altKey.addEventListener("click", function (e) { onAltKeyClick() });
 
-    changeKey(NORMAL_CHARACTER);
+    changeKey(charset.NORMAL_CHARACTER);
 });
